89214c9cdd42eb42d6d966d907dd65eac4e3c292
Alex Kersten <alex@dividebyxero.com>
2014-08-30 14:11:11 -0500

    file size check just to be safe

--- action/club-backend.py
+++ action/club-backend.py
@@ -1,5 +1,5 @@
 from bottle import error, post, run, request
-
+import os
 
 @error(404)
 def error404(error):
@@ -10,6 +10,10 @@ def error404(error):
 def interested():
     """Add user information to our text file of interested users."""
     print("New interested user: " + request.forms.yourname + " email " + request.forms.wiscmail)
+    statinfo = os.stat('interested.txt')
+    if statinfo.st_size > 1024 * 1024 * 1024:
+        return 'Something has gone wrong. Please send an email to akersten@wisc.edu telling him that the website is broken.'
+
     with open('interested.txt', 'a') as f:
         f.write('\n' + request.forms.wiscmail + ' ' + request.forms.yourname)
 
